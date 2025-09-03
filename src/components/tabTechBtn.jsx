212b06949101e13a2a257871fc1db4a960134574
HyOwl21306 <sora21306@gmail.com>
2025-09-03 15:07:34 +0700

    Delete src directory

--- src/components/tabTechBtn.jsx
+++
@@ -1,9 +0,0 @@
-export default function TabTechBtn({children, onSelect, isSelected}) {
-    return (
-        <>
-            <div className={isSelected ? `tabTech bg-white` : `tabTech text-white`} onClick={onSelect}>
-                {children}
-            </div>
-        </>
-    )
-}
